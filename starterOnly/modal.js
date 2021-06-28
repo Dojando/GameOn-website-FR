7f6f6c7a1ab36607f015aa54b66ba651e352c9b7
Dojando <wadja.b@hotmail.fr>
2021-06-28 19:50:16 +0200

    amelioration de la mise en forme des erreurs et ajout d'une modal de validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,8 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
 const buttonSubmit = document.getElementById('buttonSubmit');
+const formValidation = document.getElementById('form-valid');
+const buttonValidation = document.getElementById('buttonValidation');
 
 //Recuperation de chaque champs du formulaire
 const champPrenom = document.getElementById('first');
@@ -46,6 +48,11 @@ function CloseModal() {
   modalbg.style.display = "none";
 }
 
+// fermeture de la modal de validation
+buttonValidation.addEventListener("click", function() {
+  formValidation.style.display = "none";
+})
+
 
   // script pour la validation du formulaire
 
@@ -56,59 +63,102 @@ const regex_formulaire = {
   email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]+){2,4}$/i,
 }
 
-// au clic, verification des champs un par un et affichage d'une erreur si invalide
+//fonction pour ecouter les changements sur les champs prenom, nom et email
+function changeListener(champ, regex, erreur) {
+  champ.addEventListener('change', function() {
+    if (regex.test(champ.value) === false) {
+      champ.parentElement.setAttribute('data-error-visible','true');
+      erreur.style.display = "block";
+    } else {
+      champ.parentElement.setAttribute('data-error-visible','false');
+      erreur.style.display = "none";
+    }
+  })
+}
+
+// Verifications des champs directement à la perte de focus
+changeListener(champPrenom, regex_formulaire.prenom, erreurPrenom);
+changeListener(champNom, regex_formulaire.nom, erreurNom);
+changeListener(champEmail, regex_formulaire.email, erreurEmail);
+
+champNaissance.addEventListener('change', function() {
+  if (champNaissance.value.length === 0) {
+    champNaissance.parentElement.setAttribute('data-error-visible','true');
+    erreurNaissance.style.display = "block";
+  } else {
+    champNaissance.parentElement.setAttribute('data-error-visible','false');
+    erreurNaissance.style.display = "none";
+  }
+})
+
+champTournois.addEventListener('change', function() {
+  if (champTournois.value.length === 0) {
+    champTournois.parentElement.setAttribute('data-error-visible','true');
+    erreurTournois.style.display = "block";
+  } else {
+    champTournois.parentElement.setAttribute('data-error-visible','false');
+    erreurTournois.style.display = "none";
+  }
+})
+
+
+// au clic, verification des champs un par un et affichage d'une erreur si invalide, puis validation 
 buttonSubmit.addEventListener('click', function(e) {
+  e.preventDefault();
   let i = true;
-  if (regex_formulaire.prenom.test(champPrenom.value) === false) {
-    e.preventDefault;
-    erreurPrenom.style.display = "block";
-    i = false;
-  } else {
-    erreurPrenom.style.display = "none";
+
+  // fonction pour verifier les champs prenom, nom et email
+  function clickListener(champ, regex, erreur) {
+    if (regex.test(champ.value) === false) {
+      e.preventDefault();
+      champ.parentElement.setAttribute('data-error-visible','true');
+      erreur.style.display = "block";
+      i = false;
+    } else {
+      e.preventDefault();
+      champ.parentElement.setAttribute('data-error-visible','false');
+      erreur.style.display = "none";
+    }
   }
 
-  if (regex_formulaire.nom.test(champNom.value) === false) {
-    e.preventDefault;
-    erreurNom.style.display = "block";
-    i = false;
-  } else {
-    erreurNom.style.display = "none";
-  }
-
-  if (regex_formulaire.email.test(champEmail.value) === false) {
-    e.preventDefault;
-    erreurEmail.style.display = "block";
-    i = false;
-  } else {
-    erreurEmail.style.display = "none";
-  }
+  clickListener(champPrenom, regex_formulaire.prenom, erreurPrenom);
+  clickListener(champNom, regex_formulaire.nom, erreurNom);
+  clickListener(champEmail, regex_formulaire.email, erreurEmail);
 
   if (champNaissance.value.length === 0) {
-    e.preventDefault;
+    e.preventDefault();
+    champNaissance.parentElement.setAttribute('data-error-visible','true');
     erreurNaissance.style.display = "block";
     i = false;
   } else {
+    e.preventDefault();
+    champNaissance.parentElement.setAttribute('data-error-visible','false');
     erreurNaissance.style.display = "none";
   }
 
   if (champTournois.value.length === 0) {
-    e.preventDefault;
+    e.preventDefault();
+    champTournois.parentElement.setAttribute('data-error-visible','true');
     erreurTournois.style.display = "block";
     i = false;
   } else {
+    e.preventDefault();
+    champTournois.parentElement.setAttribute('data-error-visible','false');
     erreurTournois.style.display = "none";
   }
 
   if (checkboxCondition.checked === false) {
-    e.preventDefault;
+    e.preventDefault();
     erreurCondition.style.display = "block";
     i = false;
   } else {
+    e.preventDefault();
     erreurCondition.style.display = "none";
   }
 
   // Si tous les champs sont valide, un message de validation s'affiche
   if (i === true) {
-    window.alert("Merci ! Votre réservation a été reçue.");
+    e.preventDefault();
+    formValidation.style.display = "block";
   }
 })
